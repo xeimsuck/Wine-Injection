61ab6603d5d8e007efe80dedbe794e6d4fedbf15
xeim <xeimdonot@gmail.com>
2024-06-02 20:01:48 +0500

    create cheat-thread

--- wine/source/main.cpp
+++ wine/source/main.cpp
@@ -1,10 +1,23 @@
 #include <windows.h>
 
+// Launch cheat
+static void Start(HMODULE hDll) {
+
+}
+
+// Stop cheat
+static void Stop() {
+
+}
+
 BOOL WINAPI DllMain(HMODULE hDll, const DWORD reason, LPVOID) {
     switch(reason){
         case DLL_PROCESS_ATTACH:
-                break;
+            CreateThread(nullptr, 0, reinterpret_cast<LPTHREAD_START_ROUTINE>(Start),
+                hDll, NULL, nullptr);
+            break;
         case DLL_PROCESS_DETACH:
+            Stop();
             break;
         case DLL_THREAD_ATTACH:
             break;
