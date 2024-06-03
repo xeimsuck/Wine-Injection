4df0eeadc32c827e84e571148dafa3632d981d6d
xeim <xeimdonot@gmail.com>
2024-06-03 21:22:25 +0500

    add glfw and glew initialize+uninitialize

--- wine/source/gui/gui.cpp
+++ wine/source/gui/gui.cpp
@@ -1,9 +1,19 @@
 #include "gui.h"
 
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
 int wine::gui::initGUI() {
+    // GLFW initialization
+    if(!glfwInit()) return 1;
+
+    // GLEW initialization
+    if(glewInit()) return 1;
+
     return 0;
 }
 
 void wine::gui::destroyGUI() {
+    glfwTerminate();
 }
 
